df0edbfb199ca08fd9fe3a4175e1952ffb9102bc
YogeshkannanN <87962193+YogeshkannanN@users.noreply.github.com>
2021-12-18 12:38:23 +0530

    Add files via upload

---
+++ ListItems.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './ListItems.css';
+
+
+function ListItems(props){
+    const items = props.items;
+    const listItems = items.map(item =>
+   {
+       return <div className="list" key={item.key}>
+    <p> {item.text}
+    
+    <span>
+        <crossIcon className="icons" icon='trash'
+        onClick ={ () => props.deleteItem(item.key)}
+        />
+    </span>
+     </p>
+     
+    </div>})
+    return <div>
+    
+        {listItems}
+        
+    
+    </div>;
+  }
+
+  export default ListItems;
